4d1eb01b2565eccf961d1089aa837fe7ff3b07bc
Ganesh-RB <ganeshbombatkar777@gmail.com>
2024-04-23 19:12:15 +0530

    Updated attention code

---
+++ src/goodnews/model/attention.py
@@ -0,0 +1,204 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import torch.nn.functional as F
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class EncoderCNN(nn.Module):
+    def __init__(self):
+        super(EncoderCNN, self).__init__()
+        resnet = models.resnet50(pretrained=True)
+        for param in resnet.parameters():
+            param.requires_grad_(False)
+
+        modules = list(resnet.children())[:-2]
+        self.resnet = nn.Sequential(*modules)
+
+    def forward(self, images):
+        features = self.resnet(images)  #(batch_size,2048,7,7)
+        features = features.permute(0, 2, 3, 1)  #(batch_size,7,7,2048)
+        features = features.view(features.size(0), -1, features.size(-1))  #(batch_size,49,2048)
+        return features
+
+
+class EncoderBert(nn.Module):
+    def __init__(self, embed_size, bert_model):
+        super(EncoderBert, self).__init__()
+        self.bert = bert_model
+
+    def forward(self, articles):
+        padded = articles
+        attention_mask = np.where(padded != 0, 1, 0)
+        # Getting vectors
+        input_ids = torch.tensor(padded)
+        attention_mask = torch.tensor(attention_mask)
+
+        with torch.no_grad():
+            last_hidden_states = self.bert(input_ids, attention_mask=attention_mask)
+
+        features = last_hidden_states[0]
+        return torch.Tensor(features)
+
+
+#Bahdanau Attention
+class Attention(nn.Module):
+    def __init__(self, encoder_dim, decoder_dim, attention_dim):
+        super(Attention, self).__init__()
+
+        self.attention_dim = attention_dim
+
+        self.W = nn.Linear(decoder_dim, attention_dim)
+        self.U = nn.Linear(encoder_dim, attention_dim)
+
+        self.A = nn.Linear(attention_dim, 1)
+
+    def forward(self, features, hidden_state):
+        u_hs = self.U(features)  #(batch_size,num_layers,attention_dim)
+        w_ah = self.W(hidden_state)  #(batch_size,attention_dim)
+
+        combined_states = torch.tanh(u_hs + w_ah.unsqueeze(1))  #(batch_size,num_layers,attemtion_dim)
+
+        attention_scores = self.A(combined_states)  #(batch_size,num_layers,1)
+        attention_scores = attention_scores.squeeze(2)  #(batch_size,num_layers)
+
+        alpha = F.softmax(attention_scores, dim=1)  #(batch_size,num_layers)
+
+        attention_weights = features * alpha.unsqueeze(2)  #(batch_size,num_layers,features_dim)
+        attention_weights = attention_weights.sum(dim=1)  #(batch_size,num_layers)
+
+        return alpha, attention_weights
+
+
+#Attention Decoder
+class DecoderRNN(nn.Module):
+    def __init__(self, embed_size, vocab_size, attention_dim, encoder_dim, bert_encoder_dim, decoder_dim,
+                 drop_prob=0.3):
+        super().__init__()
+
+        #save the model param
+        self.vocab_size = vocab_size
+        self.attention_dim = attention_dim
+        self.decoder_dim = decoder_dim
+        self.bert_encoder_dim = bert_encoder_dim
+
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.attention = Attention(encoder_dim, decoder_dim, attention_dim)
+        self.para_attention = Attention(bert_encoder_dim, decoder_dim, attention_dim)
+
+        self.init_h = nn.Linear(encoder_dim + bert_encoder_dim, decoder_dim)
+        self.init_c = nn.Linear(encoder_dim + bert_encoder_dim, decoder_dim)
+        self.lstm_cell = nn.LSTMCell(embed_size + encoder_dim + bert_encoder_dim, decoder_dim, bias=True)
+        self.f_beta = nn.Linear(decoder_dim, encoder_dim + bert_encoder_dim)
+
+        self.fcn = nn.Linear(decoder_dim, vocab_size)
+        self.drop = nn.Dropout(drop_prob)
+
+    def forward(self, features, captions, articles):
+
+        #vectorize the caption
+        embeds = self.embedding(captions)
+
+        # Initialize LSTM state
+        h, c = self.init_hidden_state(features, articles)  # (batch_size, decoder_dim)
+
+        #get the seq length to iterate
+        seq_length = len(captions[0]) - 1  #Exclude the last one
+        batch_size = captions.size(0)
+        num_features = features.size(1)
+        para_features_num = articles.size(1)
+
+        preds = torch.zeros(batch_size, seq_length, self.vocab_size).to(device)
+        alphas = torch.zeros(batch_size, seq_length, num_features).to(device)
+        para_alphas = torch.zeros(batch_size, seq_length, para_features_num).to(device)
+
+        for s in range(seq_length):
+            alpha, context = self.attention(features, h)
+            para_alpha, para_context = self.para_attention(articles, h)
+
+            lstm_input = torch.cat((embeds[:, s], context, para_context), dim=1)
+            h, c = self.lstm_cell(lstm_input, (h, c))
+
+            output = self.fcn(self.drop(h))
+
+            preds[:, s] = output
+            alphas[:, s] = alpha
+            para_alphas[:, s] = para_alpha
+
+        return preds, alphas, para_alphas
+
+    def generate_caption(self, features, articles, max_len=20, vocab=None):
+        # Inference part
+        # Given the image features generate the captions
+
+        batch_size = features.size(0)
+        h, c = self.init_hidden_state(features, articles)  # (batch_size, decoder_dim)
+
+        alphas = []
+
+        #starting input
+        word = torch.tensor(vocab.word2idx['<start>']).view(1, -1).to(device)
+        embeds = self.embedding(word)
+
+        captions = []
+
+        for i in range(max_len):
+            alpha, context = self.attention(features, h)
+
+            #store the apla score
+            alphas.append(alpha.cpu().detach().numpy())
+
+            lstm_input = torch.cat((embeds[:, 0], context), dim=1)
+            h, c = self.lstm_cell(lstm_input, (h, c))
+            output = self.fcn(self.drop(h))
+            output = output.view(batch_size, -1)
+
+            #select the word with most val
+            predicted_word_idx = output.argmax(dim=1)
+
+            #save the generated word
+            captions.append(predicted_word_idx.item())
+
+            #end if <EOS detected>
+            if vocab.idx2word[predicted_word_idx.item()] == "<end>":
+                break
+
+            #send generated word as the next caption
+            embeds = self.embedding(predicted_word_idx.unsqueeze(0))
+
+        #covert the vocab idx to words and return sentence
+        return [vocab.idx2word[idx] for idx in captions], alphas
+
+    def init_hidden_state(self, encoder_out, bert_encoder_out):
+        mean_encoder_out = encoder_out.mean(dim=1)
+        mean_bert_encoder_out = bert_encoder_out.mean(dim=1)
+        print(mean_encoder_out.shape, mean_bert_encoder_out.shape)
+        inp = torch.cat((mean_encoder_out, mean_bert_encoder_out), 1)  #TODO: check for cat dim argument severty:low
+        h = self.init_h(inp)  # (batch_size, decoder_dim)
+        c = self.init_c(inp)
+        return h, c
+
+
+class EncoderDecoder(nn.Module):
+    def __init__(self, embed_size, vocab_size, attention_dim, encoder_dim, bert_encoder_dim, decoder_dim, drop_prob=0.3,
+                 bert_model=None):
+        super().__init__()
+        self.encoder = EncoderCNN()
+        self.bert_encoder = EncoderBert(embed_size, bert_model)
+        self.decoder = DecoderRNN(
+            embed_size=embed_size,
+            vocab_size=vocab_size,
+            attention_dim=attention_dim,
+            encoder_dim=encoder_dim,
+            bert_encoder_dim=bert_encoder_dim,
+            decoder_dim=decoder_dim,
+            drop_prob=drop_prob
+        )
+
+    def forward(self, images, captions, articles):
+        features = self.encoder(images)
+        bert_out = self.bert_encoder(articles)
+        outputs = self.decoder(features, captions, bert_out)
+        return outputs
---
+++ src/goodnews/model/cnn_lstm.py
@@ -0,0 +1,139 @@
+import numpy as np
+import torch.nn as nn
+import torchvision.models as models
+
+
+class EncoderCNN(nn.Module):
+    def __init__(self, embed_size):
+        super(EncoderCNN, self).__init__()
+        resnet = models.resnet50(pretrained=True)
+        for param in resnet.parameters():
+            param.requires_grad_(False)
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
+        self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+        self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
+
+    def forward(self, images):
+        with torch.no_grad():
+            features = self.resnet(images)
+        features = features.view(features.size(0), -1)
+        features = self.embed(features)
+        features = self.bn(features)
+        return features
+
+
+class EncoderBert(nn.Module):
+    def __init__(self, embed_size, bert_model):
+        super(EncoderBert, self).__init__()
+        self.bert = bert_model
+
+    def forward(self, articles):
+        padded = articles
+        attention_mask = np.where(padded != 0, 1, 0)
+        # Getting vectors
+        input_ids = torch.tensor(padded)
+        attention_mask = torch.tensor(attention_mask)
+
+        with torch.no_grad():
+            last_hidden_states = self.bert(input_ids, attention_mask=attention_mask)
+        last_hidden_states['last_hidden_state'].size()
+
+        features = last_hidden_states[0][:, 0, :].numpy()
+        return torch.Tensor(features)
+
+
+class Encoder(nn.Module):
+    def __init__(self, embed_size, bert_model):
+        super(Encoder, self).__init__()
+        self.image_encoder = EncoderCNN(embed_size)
+        self.article_encoder = EncoderBert(embed_size, bert_model)
+
+    def forward(self, images, articles):
+        image_features = self.image_encoder(images)
+        article_features = self.article_encoder(articles)
+        return torch.cat((image_features, article_features), 1)
+
+
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+class DecoderRNN(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+        """Set the hyper-parameters and build the layers."""
+        super(DecoderRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+
+    def forward(self, features, captions):
+        """Decode image feature vectors and generates captions."""
+        captions = captions[:, :-1]
+        embeddings = self.embed(captions)
+        inputs = torch.cat((features.unsqueeze(1), embeddings), 1)
+        hidden, _ = self.lstm(inputs)
+        outputs = self.linear(hidden)
+        return outputs
+
+    def sample(self, inputs, states=None, max_len=20):
+        """Accept a pre-processed image tensor (inputs) and return predicted
+        sentence (list of tensor ids of length max_len). This is the greedy
+        search approach.
+        """
+        sampled_ids = []
+        for i in range(max_len):
+            hiddens, states = self.lstm(inputs, states)
+            outputs = self.linear(hiddens.squeeze(1))
+            # Get the index (in the vocabulary) of the most likely integer that
+            # represents a word
+            predicted = outputs.argmax(1)
+            sampled_ids.append(predicted.item())
+            inputs = self.embed(predicted)
+            inputs = inputs.unsqueeze(1)
+        return sampled_ids
+
+    def sample_beam_search(self, inputs, states=None, max_len=20, beam_width=5):
+        """Accept a pre-processed image tensor and return the top predicted
+        sentences. This is the beam search approach.
+        """
+        # Top word idx sequences and their corresponding inputs and states
+        idx_sequences = [[[], 0.0, inputs, states]]
+        for _ in range(max_len):
+            # Store all the potential candidates at each step
+            all_candidates = []
+            # Predict the next word idx for each of the top sequences
+            for idx_seq in idx_sequences:
+                hiddens, states = self.lstm(idx_seq[2], idx_seq[3])
+                outputs = self.linear(hiddens.squeeze(1))
+                # Transform outputs to log probabilities to avoid floating-point
+                # underflow caused by multiplying very small probabilities
+                log_probs = F.log_softmax(outputs, -1)
+                top_log_probs, top_idx = log_probs.topk(beam_width, 1)
+                top_idx = top_idx.squeeze(0)
+                # create a new set of top sentences for next round
+                for i in range(beam_width):
+                    next_idx_seq, log_prob = idx_seq[0][:], idx_seq[1]
+                    next_idx_seq.append(top_idx[i].item())
+                    log_prob += top_log_probs[0][i].item()
+                    # Indexing 1-dimensional top_idx gives 0-dimensional tensors.
+                    # We have to expand dimensions before embedding them
+                    inputs = self.embed(top_idx[i].unsqueeze(0)).unsqueeze(0)
+                    all_candidates.append([next_idx_seq, log_prob, inputs, states])
+            # Keep only the top sequences according to their total log probability
+            ordered = sorted(all_candidates, key=lambda x: x[1], reverse=True)
+            idx_sequences = ordered[:beam_width]
+        return [idx_seq[0] for idx_seq in idx_sequences]
+
+
+class EncoderDecoder(nn.Module):
+    def __init__(self, embed_size, vocab_size, decoder_dim,                  bert_model=None):
+        super().__init__()
+        self.encoder = Encoder(embed_size, bert_model)
+        self.decoder = DecoderRNN(embed_size, decoder_dim, vocab_size)
+
+    def forward(self, images, captions, articles):
+        features = self.encoder(images, articles)
+        outputs = self.decoder(features, captions)
+        return outputs
